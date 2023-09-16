d680684ffad2be85d896ca03560e39b7cee51dc2
amayarempel <145226564+amayarempel@users.noreply.github.com>
2023-09-16 12:21:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+{
+    window.location.href = 'https://www.google.com'; // Redirect to Google for 5-star ratings.
+}
+
+function redirectToFeedback() {
+    window.location.href = 'feedback.html'; // Redirect to feedback form for lower ratings.
+}
